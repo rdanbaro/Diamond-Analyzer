d08a5fbfc60ea6cf388916970d61929a066ae3de
rdanbaro <138162038+rdanbaro@users.noreply.github.com>
2024-02-10 23:18:14 -0500

    Base Vista general + sprint guardados +  vista review(primer sprint)

--- src/controllers/controladora_principal.py
+++ src/controllers/controladora_principal.py
@@ -1,29 +1,32 @@
 from views.ui_vista_principal import Ui_MainWindow
 from controllers.controladora_formulario import Formulario
-from PySide6.QtWidgets import QMainWindow, QWidget
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
 from controllers.controladora_review import SprintReview
 from controllers.controladora_vista_inicio import VistaInicio
+from controllers.controladora_vista_general import VistaGeneral
 
 class VistaPrincipal(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         # self.ui = Ui_MainWindow()
         self.setupUi(self)
-    
+        
         self.vista_inicio = VistaInicio()
         self.vista_formulario = Formulario()
         self.vista_review = SprintReview()
+        self.vista_general = VistaGeneral()
 
         self.vista_inicio.show()
 
+
+        self.vista_inicio.sprintAnalysisButton.clicked.connect(self.mostrar_form)
+        self.vista_inicio.statsButton.clicked.connect(self.mostrar_vista_general)
         self.vista_inicio.exitButton.clicked.connect(self.cerrar_inicio)
-        self.vista_inicio.sprintAnalysisButton.clicked.connect(self.sprint_analysis)
+        
 
 
 
-
-
-        self.contenedor_form.addWidget(self.vista_formulario)
+       
 
         self.vista_formulario.boton_aceptar.clicked.connect(
             self.validar_form)
@@ -35,24 +38,28 @@ class VistaPrincipal(QMainWindow, Ui_MainWindow):
         self.vista_review.Boton_Siguiente.clicked.connect(self.sprint_analysis_siguiente)
 
 
-
-    #Vista Sprint Review
-    def sprint_analysis(self):
+    #Métodos Vista de Inicio
+    def mostrar_form(self):
         self.show()
+        self.contenedor_form.addWidget(self.vista_formulario)
         self.vista_inicio.close()
-    
-    def sprint_analysis_siguiente(self):
-        self.contenedor_form.removeWidget(self)
-        self.vista_review.deleteLater()
 
-
-
-
+    def mostrar_vista_general(self):
+        self.vista_inicio.close()
+        self.vista_general.mostrar_sprint_guardados()
+        self.contenedor_form.addWidget(self.vista_general)
+        self.showMaximized()
+        
 
     def cerrar_inicio(self):
         self.vista_inicio.close()
     
+
+
     
+    
+
+    #Métodos Vista Formulario
     def validar_form(self):
         validador, nombre_sprint, tp_sprint, ruta_objs, ruta_habitos, ruta_diamond, ruta_entreno = self.vista_formulario.form_validator() 
         if validador:  
@@ -63,6 +70,19 @@ class VistaPrincipal(QMainWindow, Ui_MainWindow):
     
     def cancelar_form(self):
         self.close()
+        self.contenedor_form.removeWidget(self)
+        self.vista_formulario.deleteLater()
         self.vista_inicio.show()
 
     
+    
+    
+    #Métodos Vista Sprint Review
+    def sprint_analysis_siguiente(self):
+        self.contenedor_form.removeWidget(self)
+        self.vista_review.deleteLater()
+        self.close()
+        self.vista_general.mostrar_sprint_guardados()
+        self.contenedor_form.addWidget(self.vista_general)
+        self.showMaximized()
+        
---
+++ src/controllers/controladora_vista_general.py
@@ -0,0 +1,64 @@
+from PySide6.QtCore import Qt
+from views.ui_vista_general import Ui_GeneralView
+from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout
+import database
+from models.modelos import Sprint
+from controllers.controladora_review import SprintReview
+
+
+class VistaGeneral(QWidget, Ui_GeneralView):
+    def __init__(self):
+        super().__init__()
+        
+        self.setupUi(self)
+        self.lista_botones = []
+
+        self.layout_contenedor_metas = QVBoxLayout()
+        self.tab_metas.setLayout(self.layout_contenedor_metas)
+        
+        self.layout_sprints = QVBoxLayout()
+        self.layout_sprints.setAlignment(Qt.AlignTop)
+        self.widget_2 = QWidget()
+        self.widget_2.setLayout(self.layout_sprints)
+        #self.widget_2.setObjectName(u"widget_2")
+        
+        
+        self.scrollArea.setWidget(self.widget_2)
+        
+        self.vista_metas = SprintReview()
+          
+        
+
+    def mostrar_sprint(self, titulo):
+        sprint = database.sesion.query(Sprint).filter(Sprint.nombre_sprint == titulo).first()
+        self.vista_metas.dame_info_view(sprint.nombre_sprint, sprint.tipo, sprint.data_metas_objetivos, sprint.data_Habitos, sprint.data_Diamantes, sprint.data_Entrenamiento)
+        self.layout_contenedor_metas.addWidget(self.vista_metas)
+        
+        
+
+        
+
+
+
+    def mostrar_sprint_guardados(self):
+        sprints = database.sesion.query(Sprint).all()
+        for sprint in sprints:
+            button = QPushButton(sprint.nombre_sprint)
+            button.setMinimumSize(265,30)
+            
+            # Configurar el botón según sea necesario
+            # Por ejemplo, conectarlo a una función de manejo de clic
+            
+            self.lista_botones.append(button)
+
+            # button.clicked.connect(lambda checked, nombre_sprint=sprint.nombre_sprint: self.mostrar_prompt(nombre_sprint))
+            # self.scrollAreaWidgetContents.layout().addWidget(button)
+            # self.lista_botones.append(button)
+
+        list(map(lambda i: self.layout_sprints.addWidget(i), self.lista_botones))
+        boton_texto = self.lista_botones[0].text()
+        print(boton_texto)
+        self.lista_botones[0].clicked.connect(lambda: self.mostrar_sprint(boton_texto))
+        
+        # list(map(lambda boton: boton.clicked.connect(
+        #     lambda: self.mostrar_sprint(boton.text())), self.lista_botones))
